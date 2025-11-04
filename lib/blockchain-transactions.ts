69b1d07c2e87a164984923ca61d9f8639d095b65
Ridwan O. Abdullateef <49338477+readone97@users.noreply.github.com>
2025-11-04 04:59:49 -0800

    Update blockchain-transactions.ts

--- lib/blockchain-transactions.ts
+++ lib/blockchain-transactions.ts
@@ -1,6 +1,6 @@
 import { Connection, PublicKey } from "@solana/web3.js";
 
-const RPC_ENDPOINT = "https://serene-wispy-model.solana-mainnet.quiknode.pro/2ebdf944147ac60d02e7030145216e4e1681dd2c/";
+const RPC_ENDPOINT = "https://late-few-resonance.solana-mainnet.quiknode.pro/5d429d69e9f9dd861ceecd843b57aa15a3441139/";
 
 export interface BlockchainTransaction {
   signature: string;
@@ -129,3 +129,4 @@ export function detectTransactionType(transaction: BlockchainTransaction): "swap
 
   return "other";
 }
+
